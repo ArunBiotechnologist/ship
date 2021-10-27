33e8dfcaebd4bb548cf8ea87a8c694a4d69b00b0
ArunBiotechnologist <63848670+ArunBiotechnologist@users.noreply.github.com>
2021-10-27 10:40:06 +0530

    Add files via upload

---
+++ semantic_app_incomplete.R
@@ -0,0 +1,122 @@
+
+library(shiny)
+library(shiny.semantic)
+library(dplyr)
+library(leaflet)
+
+
+# Read the file
+ship_table1 <- read.csv("ships.csv")
+
+# Remove the observations for parked ships
+ship_table <- ship_table1 %>% filter(is_parked == 0)
+
+
+# Function to calculate distance in meters
+# https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
+measure <- function(lon1,lat1,lon2,lat2) {
+
+    R <- 6378.137                                # radius of earth in Km
+    dLat <- (abs(lat2-lat1))*pi/180
+    dLon <- (abs(lon2-lon1))*pi/180
+    a <- sin((dLat/2))^2 + cos(lat1*pi/180)*cos(lat2*pi/180)*(sin(dLon/2))^2
+    c <- 2 * atan2(sqrt(a), sqrt(1-a))
+    d <- R * c
+    return (d * 1000)                            # distance in meters
+}
+
+
+# Define UI for application 
+ui <- semanticPage(
+	
+	theme = "cerulean",
+    titlePanel("Ship Information Resource"),
+	sidebarLayout(
+		sidebarPanel(
+			segment(
+			h4("The time is ",
+        textOutput("currentTime", container = span)),
+			br(),
+			dropdown_input("type", default_text = h3("Select Vessel Type:"), 
+   			 choices = unique(ship_table$ship_type), 
+   			 value = unique(ship_table$ship_type)[1]),
+
+			dropdown_input("name", default_text = h3("Choose Ship:"), 
+   			 choices = "KAROLI"
+					),
+
+			br(),
+			br(),
+			br(),
+			br(),
+			h5("WebApp designed by"),
+			tags$a(href="https://orcid.org/0000-0001-5686-640X", "Arun Arumugaperumal"),
+			br(),
+			br(),
+			br(),
+			br()
+  			   ) # close segment
+				),
+		mainPanel(
+				segment(
+						textOutput("text", container = span),		
+						leaflet::leafletOutput("map")
+					   )
+			    )#close mainPanel
+				)#close sidebarLayout
+			)#close fluidpage
+
+    
+
+# Define server 
+server <- function(input, output, session) {
+
+	 observeEvent(input$type, {
+    			update_dropdown_input(session, input_id = "name", choices = ship_table %>% 
+				filter(ship_type == input$type)%>%
+				summarize(names  = unique(SHIPNAME)))
+
+				})
+
+	 observeEvent(input$name, {
+
+			# Subset the dataframe
+				df <- ship_table[ship_table$SHIPNAME == input$name,]	
+#Calculate distances
+ms <- dim(df)[1] -1
+mh <- vector("numeric")
+for(i in 1:ms){
+mh[i] <- measure(df$LON[i], df$LAT[i], df$LON[i+1], df$LAT[i+1])
+mh <- c(mh, mh[i])
+		    }
+mh_max <- max(mh)
+
+			output$text <- renderText({
+				paste("The distance sailed by",input$name, "is", mh_max, "meters")
+								})
+
+
+# Select the maximum traveelled locations
+mh_max_index <- which(mh == mh_max)
+
+output$map <- leaflet::renderLeaflet({
+leaflet() %>% 
+    addTiles() %>% 
+    addMarkers(lat = c(df$LAT[mh_max_index:(mh_max_index+1)]), lng = c(df$LON[mh_max_index:(mh_max_index+1)]), popup=df$DATETIME[mh_max_index:(mh_max_index+1)]) %>%
+    addPolylines(lat = c(df$LAT[mh_max_index:(mh_max_index+1)]), lng = c(df$LON[mh_max_index:(mh_max_index+1)]))
+})
+	
+				})
+				
+	output$currentTime <- renderText({
+		invalidateLater(1, session)
+		format(Sys.time())
+  				})
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
